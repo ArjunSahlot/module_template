e170feabe53e28783e9797ec02026995d3f58045
Arjun <44180430+ArjunSahlot@users.noreply.github.com>
2021-04-27 08:59:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md") as f:
+    long_description = f.read()
+
+with open("requirements.txt") as f:
+    reqs = f.read().strip().split("\n")
 
 setuptools.setup(
     name="module",
     version="0.0.1",
     author="Arjun Sahlot",
     author_email="iarjun.sahlot@gmail.com",
-    description="Module with various useful utilities.",
+    description="Template for module",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license="GNU GPL v3",
     url="https://github.com/ArjunSahlot/",
+    keywords=["Module"],
     py_modules=["module"],
     packages=setuptools.find_packages(),
-    install_requires=[
-        "numpy"
-    ],
+    install_requires=reqs,
+    python_requires=">=3",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
